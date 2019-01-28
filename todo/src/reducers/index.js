1bc643581fb9cfe2965f4cf40dbf38f2511c1d35
Nathan Thomas <nwthomas@me.com>
2019-01-28 13:00:18 -0800

    Make data in todo list persist through refreshes

--- todo/src/reducers/index.js
+++ todo/src/reducers/index.js
@@ -1,24 +1,30 @@
 import { ADD_TODO, TOGGLE_COMPLETED, CLEAR_TODO } from "../actions";
 
+const data = [
+  {
+    task: "Do laundry",
+    id: 798012347890123,
+    completed: false
+  },
+  {
+    task: "Walk the dog",
+    id: 980713487098711,
+    completed: false
+  },
+  {
+    task: "Take a nap",
+    id: 123094897987138,
+    completed: false
+  }
+];
+
+const todos = JSON.parse(localStorage.getItem("todos"))
+  ? JSON.parse(localStorage.getItem("todos"))
+  : data;
+
 const initialState = {
   whatever: "value",
-  todos: [
-    {
-      task: "Do laundry",
-      id: 798012347890123,
-      completed: false
-    },
-    {
-      task: "Walk the dog",
-      id: 980713487098711,
-      completed: false
-    },
-    {
-      task: "Take a nap",
-      id: 123094897987138,
-      completed: false
-    }
-  ]
+  todos: todos
 };
 
 function reducer(state = initialState, action) {
@@ -29,27 +35,31 @@ function reducer(state = initialState, action) {
           ? { ...todo, completed: !todo.completed }
           : todo;
       });
+      localStorage.setItem("todos", JSON.stringify(updatedTodo));
       return {
         ...state,
         todos: updatedTodo
       };
     case CLEAR_TODO:
       const clearedTodos = state.todos.filter(todo => todo.completed === false);
+      localStorage.setItem("todos", JSON.stringify(clearedTodos));
       return {
         ...state,
         todos: clearedTodos
       };
     case ADD_TODO:
+      const updatedTodos = [
+        ...state.todos,
+        {
+          task: action.payload,
+          id: Date.now(),
+          completed: false
+        }
+      ];
+      localStorage.setItem("todos", JSON.stringify(updatedTodos));
       return {
         ...state,
-        todos: [
-          ...state.todos,
-          {
-            task: action.payload,
-            id: Date.now(),
-            completed: false
-          }
-        ]
+        todos: updatedTodos
       };
     default:
       return state;
